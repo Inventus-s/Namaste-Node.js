d74e6c6b429af00e90420f67ab2185f18a15e987
Mukund Kumar <123208378+Inventus-s@users.noreply.github.com>
2026-04-27 23:46:37 +0530

    solved minimum of three using if-else

--- Practice/conditional.js
+++ Practice/conditional.js
@@ -1,7 +1,25 @@
 // Find even and odd number
-let number = 54;
-if (number % 2 === 0) {
-    console.log(number + " is an even number");
+// let number = 54;
+// if (number % 2 === 0) {
+//     console.log(number + " is an even number");
+// } else {
+//     console.log(number + " is an odd number");
+// }
+
+// Find minimum of three
+let x = 10, y = 3, z = 10;
+if(x < y) {
+    if(x < z) {
+        console.log(x + " is the minimum of three");
+    } else if(y < z) {
+        console.log(x + " is the minimum of three");
+    } else {
+        console.log(z + " is the minimum of three");
+    }
+} else if (x < z) {
+    console.log(y + " is the minimum of three"); 
+} else if (y < z) {
+    console.log(y + " is the minimum of three"); 
 } else {
-    console.log(number + " is an odd number");
+    console.log(z + " is the minimum of three");
 }
